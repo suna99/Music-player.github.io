f7bbe43c13f7b48d18226e186c315747ff8acf02
sunachoiAAA <102567671+sunachoiAAA@users.noreply.github.com>
2022-09-19 21:27:28 +0900

    2022.09.19

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Wager With Angels",
+    artist: "Nathan Moore",
+    img: "img01",
+    src: "music01"
+  },
+  {
+    name: "Glass",
+    artist: "Anno Domini Beats",
+    img: "img02",
+    src: "music02"
+  },
+  {
+    name: "Kind of a Party",
+    artist: "Mini Vandals",
+    img: "img03",
+    src: "music03"
+  },
+  {
+    name: "Sailing",
+    artist: "Telecasted",
+    img: "img04",
+    src: "music04"
+  },
+  {
+    name: "Natural Light",
+    artist: "Chris Haugen",
+    img: "img05",
+    src: "music05"
+  },
+  {
+    name: "Positive Fuse",
+    artist: "French Fuse",
+    img: "img06",
+    src: "music06"
+  }
+]
